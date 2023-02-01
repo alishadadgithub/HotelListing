f32cdac4e7cf1b8ecf6cd0df8eb8682519c2f915
ali.shadad <alishadad2016@gmail.com>
2023-02-01 20:07:06 +0200

    Add project files.

---
+++ Controllers/Data/Country.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelListing.Controllers.Data
+{
+    public class Country
+    {
+
+        [Key] public int Id { get; set; }
+        [Required] public string Name { get; set; } = string.Empty;
+        [Required] public string ShortName { get;set; } = string.Empty;
+    }
+}
---
+++ Controllers/Data/Hotel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.Contracts;
+using System.Security.Principal;
+
+namespace HotelListing.Controllers.Data
+{
+    public class Hotel
+    {
+
+        [Key] public int Id { get; set; }
+        [Required] public string Name { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public double Rating { get; set; }
+
+
+
+
+        [ForeignKey(nameof(Country))]
+        public int CountryId { get; set; }
+        public Country? Country { get; set; } 
+
+
+    }
+}
+ 
