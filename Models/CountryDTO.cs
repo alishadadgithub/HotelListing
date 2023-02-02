63514c7c25b9094cf8d3c82abf80a216ec2b1934
alishadadgithub <72918115+alishadadgithub@users.noreply.github.com>
2023-02-02 12:40:49 +0200

    f

---
+++ Models/CountryDTO.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelListing.Models
+{
+    public class CountryDTO : CreateCountryDTO
+    {
+
+        public int Id { get; set; }
+
+         
+    }
+
+    //WITHOUT ID
+    public class CreateCountryDTO
+    {
+        [Required]
+        [StringLength(maximumLength: 50, ErrorMessage = "country name is too long")]
+        public string Name { get; set; } = string.Empty;
+
+
+        [Required]
+        [StringLength(maximumLength: 2, ErrorMessage = "country short  is too long")]
+        public string ShortName { get; set; } = string.Empty;
+
+    }
+
+
+
+
+}
---
+++ Models/HotelDTO.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelListing.Models
+{
+    public class HotelDTO
+    {
+
+         public int Id { get; set; }
+
+        [Required]
+        [StringLength(30,ErrorMessage ="TOO LONG")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100, ErrorMessage = "TOO LONG")]
+        public string Address { get; set; } = string.Empty;
+
+        [Required]
+        [Range(1,5)]
+        public double Rating { get; set; }
+
+
+        [Required]
+        public int CountryId { get; set; }
+    }
+
+
+    public class CreateHotelDTO
+    {
+
+        
+
+        [Required]
+        [StringLength(30, ErrorMessage = "TOO LONG")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100, ErrorMessage = "TOO LONG")]
+        public string Address { get; set; } = string.Empty;
+
+        [Required]
+        [Range(1, 5)]
+        public double Rating { get; set; }
+
+
+        [Required]
+        public int CountryId { get; set; }
+    }
+}
